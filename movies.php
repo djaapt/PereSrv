5d9778d27b84107bd2f7d00f5f58560323815cd0
Rocketcandy <andrewthooft@gmail.com>
2014-03-21 19:58:35 -0500

    Update to pull movies into movie page

--- movies.php
+++ movies.php
@@ -4,6 +4,24 @@ include 'header.php'; ?>
 
 <div id="info">
 Movies
+<?php
+//Build the connection to SQL server
+include '/media/dbinfo.php';
+
+//DB connection variable to call later
+$TABLE = "movies";
+$DBC = mysqli_connect($HOST,$USER,$PASS,$DBASE) or die ('Unable to select Database');
+
+//Build query to check for duplicates already in the database
+$QUERY = "SELECT moviename FROM $TABLE";
+$GET = mysqli_query($DBC,$QUERY);
+while($ROW = mysqli_fetch_array($GET)){
+$MEDIA = $ROW['moviename'];
+$PATH = "./Disk4/2010's".$MEDIA;
+echo '<a href="'.$PATH.'">'.$MEDIA.'</a></br>';
+}
+
+mysqli_close($DBC);
 
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
 include 'footer.php'; ?>
