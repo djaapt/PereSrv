ef675677f29b546cdd2e6c01b08f7f3a73e709da
Rocketcandy <andrewthooft@gmail.com>
2014-03-21 21:09:13 -0500

    show music on home page

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ include 'header.php'; ?>
 include '/media/dbinfo.php';
 
 //DB connection variable to call later
+$TABLE = "tvshow";
 $DBC = mysqli_connect($HOST,$USER,$PASS,$DBASE) or die ('Unable to select Database');
 
 $query="SELECT showname FROM $TABLE ORDER BY RAND() LIMIT 11";
@@ -37,6 +38,38 @@ if($query_run=mysqli_query($DBC,$query))
 </div>
 </div>
 <div class="bubble"></div>
+<div class="music">
+<table align="center" style="margin-left: 30px; margin-right: 30px;">
+<tr>
+<?php
+//Build the connection to SQL server
+include '/media/dbinfo.php';
+
+//DB connection variable to call later
+$TABLE = "music";
+$DBC = mysqli_connect($HOST,$USER,$PASS,$DBASE) or die ('Unable to select Database');
+
+$query="SELECT musicname FROM $TABLE ORDER BY RAND() LIMIT 11";
+if($query_run=mysqli_query($DBC,$query))
+{
+    $i=11;
+    $rows=mysqli_fetch_array($query_run);
+    while($rows=mysqli_fetch_array($query_run))
+    {
+        $PATH = "./Disk2/Music/".$rows['musicname'];
+		$IMAGEPATH = "$PATH/cover.jpg";
+		$MUSIC = $rows['musicname'];
+		echo '<td style=padding-top: 5px; padding-bottom: 5px;><a href="'.$PATH.'"><img src="'.$IMAGEPATH.'" title="'.$MUSIC.'" height=140></a></td>';
+        $i=$i-1;
+    }
+} else {
+    echo'<font color="red"> Query does not run. </font>';
+}
+?>
+</tr>
+</table>
+</div>
+</div>
 <div class="bubble"></div>
 
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
