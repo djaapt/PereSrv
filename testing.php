bd1ce660e4cdd09d6ec28e8cd02f5d0f588949e7
symalla <symalla@gmail.com>
2014-03-27 13:13:33 -0500

    not working

--- testing.php
+++ testing.php
@@ -13,7 +13,7 @@ include 'header.php'; ?>
   jwplayer("myVideo").setup({
     modes: [
         { type: 'html5' },
-        { type: 'mp4', src: '/scripts/player.swf' }
+        { type: 'mkv', src: '/scripts/player.swf' }
     ]
   });
 </script>
