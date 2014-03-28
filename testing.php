03772805af4f5c9a5c0bea6a83897abc4bfef0d8
symalla <symalla@gmail.com>
2014-03-28 13:27:16 -0500

    testing with just jwplayer

--- testing.php
+++ testing.php
@@ -2,41 +2,21 @@
 <?php //Include the header - header starts the html and body tags
 include 'header.php'; ?>
 
-<script type="text/javascript" src="/scripts/jwplayer.js"></script>
+<script type="text/javascript" src="/jwplayer/jwplayer.js"></script>
 
-<pre>
-<?php
-
-echo 'killing ffmpeg...<br/>';
-system('killall -9 ffmpeg 2>&1');
-
-echo 'starting transcoding...<br/>';
-// $PID = shell_exec("nohup ffmpeg -i rickroll.mp4 -ar 22050 -ab 32000 -f flv -s 320x240 test.flv | flvtool2 -U stdin test.flv 2> /tmp/mehe > /tmp/meh & echo $!");
-$PID = shell_exec("nohup /usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -vcodec libvpx -acodec libvorbis -aq 90 -ac 2 - 2> /tmp/mehe > /tmp/meh & echo $!");
-
-echo 'created pid: '. $PID;
-
-?>
-</pre>
-
-<div id="container"> Loading the player... </div>
+<video height="270" width="480" id="myVideo">
+  <source src="/var/www/Videos/The.Avengers.2012.mkv" type="video/mkv">
+</video>
 
 <script type="text/javascript">
-jwplayer("container").setup({
-'flashplayer': 'player.swf',
-'file': './getvideo.php?v=23749287492',
-'provider': 'video',
-'bufferlength': '5',
-'autostart': 'true',
-'controlbar': 'bottom',
-'width': '470',
-'height': '320'
-});
+  jwplayer("myVideo").setup({
+    modes: [
+        { type: 'html5' },
+        { type: 'flash', src: '/jwplayer/player.swf' }
+    ]
+  });
 </script>
 
-
-<?php
-?>
 
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
 include 'footer.php'; ?>
