78991088fd1bd3db6e9ba1718312adb4ed7998c8
symalla <symalla@gmail.com>
2014-03-28 08:01:47 -0500

    needed to make php script for test

--- testing.php
+++ testing.php
@@ -1,8 +1,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <?php //Include the header - header starts the html and body tags
 include 'header.php'; ?>
-
+<?php
 extension_loaded('ffmpeg') or die("ffmpeg extension not loaded");
-
+?>
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
 include 'footer.php'; ?>
