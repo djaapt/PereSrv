cb6ec67ba9f6f714ef32f6ed6aa06c9348a3afdd
symalla <symalla@gmail.com>
2014-03-28 11:25:45 -0500

    changing output

--- testing.php
+++ testing.php
@@ -4,12 +4,11 @@ include 'header.php'; ?>
 <?php
 extension_loaded('ffmpeg') or die('Error in loading ffmpeg');
 
-echo exec('/usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -vcodec libvpx -acodec libvorbis -aq 90 -ac 2 - 2>$1', $output);
-var_dump($output);
+//echo exec('/usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -vcodec libvpx -acodec libvorbis -aq 90 -ac 2 - 2>$1', $output);
+//var_dump($output);
 
-$cmd = 'wget -qO- "https://djaapt.com:8443/stream" | /usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -vcodec libvpx -acodec libvorbis -aq 90 -ac 2 -';
-$video = passthru($cmd); 
-echo '<video>'.$video.'</video>';
+$cmd = '/usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -vcodec libvpx -acodec libvorbis -aq 90 -ac 2 - ';
+$output = exec($cmd . ' 2>&1', $output, $return);
 ?>
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
 include 'footer.php'; ?>
