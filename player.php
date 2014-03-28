a4a138ef85dd0b9ac0bec6846fc4c682ee87f2d1
symalla <symalla@gmail.com>
2014-03-28 14:45:46 -0500

    change some of the formattting

--- player.php
+++ player.php
@@ -1,14 +1,16 @@
 <div id='mediaplayer'>Radioplayer will be in this DIV</div>
 <script type="text/javascript" src="/scripts/jwplayer.js" ></script>
 <script type="text/javascript">
-  jwplayer('mediaplayer').setup({
-    'flashplayer': './scripts/jwplayer.flash.swf',
-    'id': 'player1',
-    'type': 'sound',
-    'width': '480',
-    'height': '270',
-    'autoplay': 'true',
-    'volume': '60',
-    'streamer': 'testing.php'
+  jwplayer("mediaplayer").setup({
+    flashplayer: "./scripts/jwplayer.flash.swf",
+    id: player1,
+    type: sound,
+    width: 480,
+    height: 270,
+    autoplay: true,
+    volume: 60,
+    sources: [{
+		file: "./testing.php"
+		}]
   });
 </script>
