844a089a67475b326e6c6c89992f51929a7d3d1b
symalla <symalla@gmail.com>
2014-03-28 14:54:55 -0500

    added html tags

--- player.php
+++ player.php
@@ -1,11 +1,12 @@
+<HEAD>
 <script type="text/javascript" src="/scripts/jwplayer.js" ></script>
-
+</HEAD>
+<BODY>
 <div id="myElement">Loading the player ...</div>
 
 <script type="text/javascript">
     jwplayer("myElement").setup({
-        file: "./testing.php",
-        height: 360,
-        width: 640
+        file: "https://djaapt.com:8443/testing.php",
     });
 </script
+</BODY>
