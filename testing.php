80e3cd6992fc33539c9c1d748c5e9d3331812c99
symalla <symalla@gmail.com>
2014-03-28 10:06:06 -0500

    got rid of redirect

--- testing.php
+++ testing.php
@@ -4,8 +4,7 @@ include 'header.php'; ?>
 <?php
 extension_loaded('ffmpeg') or die('Error in loading ffmpeg');
 
-echo exec('/usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -vcodec libvpx -acodec libvorbis -aq 90 -ac 2 - 2>&1', $output);	
-var_dump($output);
+echo exec('/usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -vcodec libvpx -acodec libvorbis -aq 90 -ac 2 - 2>&1');	
 ?>
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
 include 'footer.php'; ?>
