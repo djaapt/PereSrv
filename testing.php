f95646be00b1bf2f6e5f1c109984249e208ab462
symalla <symalla@gmail.com>
2014-03-31 07:49:20 -0500

    removed unrecognized option -c

--- testing.php
+++ testing.php
@@ -2,6 +2,6 @@
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
-system('/usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -y -b:v BITRATE -vprofile baseline -threads 4 -map 0:v -map 0:a:0  -c:a libfaac -b:a 160000 -ac 2   -hls_time 10 -hls_list_size 6 -hls_wrap 18 -start_number 1 /tmp/stream.m3u8');
+system('/usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -y -b:v BITRATE -vprofile baseline -threads 4 -map 0:v -map 0:a:0  -b:a 160000 -ac 2   -hls_time 10 -hls_list_size 6 -hls_wrap 18 -start_number 1 /tmp/stream.m3u8');
 
 ?>
