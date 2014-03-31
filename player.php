89c620587c8d614f8576637d8a4f6dff6c1c0868
symalla <symalla@gmail.com>
2014-03-31 10:52:44 -0500

    adjusting player size

--- player.php
+++ player.php
@@ -2,7 +2,7 @@
 <?php //Include the header - header starts the html and body tags
 include 'header.php'; ?>
 <div id="info">
-<video height="531" width="945" autoplay="autoplay" controls>
+<video height="480" width="945" autoplay="autoplay" controls>
 <source src="Videos/10000.BC.2008.webm" type="video/webm">
 </video> 
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
