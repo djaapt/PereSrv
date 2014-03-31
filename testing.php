95300755c2143e21574614b7ce10cf3d8c8aa85d
symalla <symalla@gmail.com>
2014-03-31 08:13:39 -0500

    changing to working ffmpeg...

--- testing.php
+++ testing.php
@@ -2,6 +2,6 @@
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
-system('/usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm -y -b:v BITRATE -vprofile baseline -threads 4 -map 0:v -map 0:a:0 -b:a 160000 -ac 2 /var/www/PereSrv/stream.m3u8');
+system('/usr/bin/ffmpeg -i /var/www/PereSrv/Videos/Brave.2012.mp4 -f webm  -vcodec libvpx -acodec libvorbis -aq 90 -ac 2 /var/www/PereSrv/stream.m3u8');
 
 ?>
