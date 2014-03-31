bbfc7addab0c9b8edb9119df19edd050df751097
symalla <symalla@gmail.com>
2014-03-31 08:03:41 -0500

    removed extra spaces

--- player.php
+++ player.php
@@ -1,11 +1,9 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <?php //Include the header - header starts the html and body tags
 include 'header.php'; ?>
-
 <div id="info">
-<video  height="531"  width="945" autoplay="autoplay" controls="controls">
+<video height="531" width="945" autoplay="autoplay" controls="controls">
 <source src="stream.m3u8" />
 </video> 
-
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
 include 'footer.php'; ?>
