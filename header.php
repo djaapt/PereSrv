99fac50aa5180c48be21d07f0ce16fbaf64bf68d
symalla <symalla@gmail.com>
2014-04-03 13:48:21 -0500

    added to headder

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+<?php
+// Check if session is not registered, redirect back to main page. 
+// Put this code in first line of web page. 
+session_start();
+if(!session_is_registered(myusername)){
+header("location:main_login.php");
+}
+?>
 <html>
 <head>
 <title>peresrv</title>
