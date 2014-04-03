7b22f7ad8aa12c2ee8bfeb9444a81eb274d33d94
symalla <symalla@gmail.com>
2014-04-03 13:59:16 -0500

    making redirect go to index

--- checklogin.php
+++ checklogin.php
@@ -29,7 +29,7 @@ if($count==1){
 // Register $USER, $PASS and redirect to file "login_success.php"
 session_register("USER");
 session_register("PASS"); 
-header("location:login_success.php");
+header("location:index.php");
 }
 else {
 echo "Wrong Username or Password";
