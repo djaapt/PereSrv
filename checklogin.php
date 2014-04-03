1324bcba12b91a540639069a81320c159182d516
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-04-03 13:47:57 -0500

    Update Vars

--- checklogin.php
+++ checklogin.php
@@ -1,36 +1,34 @@
 <?php
 
-$host="localhost"; // Host name 
-$username=""; // Mysql username 
-$password=""; // Mysql password 
-$db_name="test"; // Database name 
-$tbl_name="members"; // Table name 
+//Build the connection to SQL server
+include '/media/dbinfo.php';
+$TABLE = "members"; // Table name 
 
 // Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
+mysql_connect("$HOST", "$USER", "$PASS")or die("cannot connect"); 
+mysql_select_db("$DBASE")or die("cannot select DB");
 
 // username and password sent from form 
-$myusername=$_POST['myusername']; 
-$mypassword=$_POST['mypassword']; 
+$USER=$_POST['USER']; 
+$PASS=$_POST['PASS']; 
 
 // To protect MySQL injection (more detail about MySQL injection)
-$myusername = stripslashes($myusername);
-$mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
-$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+$USER = stripslashes($USER);
+$PASS = stripslashes($PASS);
+$USER = mysql_real_escape_string($USER);
+$PASS = mysql_real_escape_string($PASS);
+$sql="SELECT * FROM $TABLE WHERE username='$USER' and password='$PASS'";
 $result=mysql_query($sql);
 
 // Mysql_num_row is counting table row
 $count=mysql_num_rows($result);
 
-// If result matched $myusername and $mypassword, table row must be 1 row
+// If result matched $USER and $PASS, table row must be 1 row
 if($count==1){
 
-// Register $myusername, $mypassword and redirect to file "login_success.php"
-session_register("myusername");
-session_register("mypassword"); 
+// Register $USER, $PASS and redirect to file "login_success.php"
+session_register("USER");
+session_register("PASS"); 
 header("location:login_success.php");
 }
 else {
