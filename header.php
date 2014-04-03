5166ef6d940bd1be9a2a1212e14fa9931ab6a39e
symalla <symalla@gmail.com>
2014-04-03 14:07:08 -0500

    this should match

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 // Check if session is not registered, redirect back to main page. 
 // Put this code in first line of web page. 
 session_start();
-if(!session_is_registered(myusername)){
+if(!session_is_registered(USER)){
 header("location:main_login.php");
 }
 ?>
