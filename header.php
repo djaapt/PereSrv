0abaa1845128611b2e5008ea556762d7535f50df
Rocketcandy <andrewthooft@gmail.com>
2014-04-03 22:41:11 -0500

    update include again

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ if(!session_is_registered(USER)){
 header("location:BASEPATH/main_login.php");
 }
 //Include config file do not include this part in every page
-include 'config.php';
+include_once 'config.php';
 ?>
 <html>
 <head>
