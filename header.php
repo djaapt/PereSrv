c560f23315e1b2c5d3ad0a90710755d938064161
Rocketcandy <andrewthooft@gmail.com>
2014-04-03 22:16:37 -0500

    Make all header paths global

--- header.php
+++ header.php
@@ -3,28 +3,28 @@
 // Put this code in first line of web page. 
 session_start();
 if(!session_is_registered(USER)){
-header("location:main_login.php");
+header("location:$BASEPATH/main_login.php");
 }
 //Include config file do not include this part in every page
-include_once 'config.php';
+include_once $BASEPATH.'/config.php';
 ?>
 <html>
 <head>
 <title>peresrv</title>
-<link rel="icon" type="image/png" href="images/favicon.png">
-<LINK href="<?php $BASEPATH; ?>style.css" rel="stylesheet" type="text/css">
+<link rel="icon" type="image/png" href="<?php $BASEPATH; ?>/images/favicon.png">
+<link href="<?php $BASEPATH; ?>/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div id="header">
 <div id='cssmenu'>
 <ul>
-   <li class='active'><a href="index.php"><span>Home</span></a></li>
-   <li><a href="movies.php"><span>Movies</span></a></li>
-   <li><a href="shows.php"><span>TVShows</span></a></li>
-   <li><a href="music.php"><span>Music</span></a></li>
+   <li class='active'><a href="<?php $BASEPATH; ?>/index.php"><span>Home</span></a></li>
+   <li><a href="<?php $BASEPATH; ?>/movies.php"><span>Movies</span></a></li>
+   <li><a href="<?php $BASEPATH; ?>/shows.php"><span>TVShows</span></a></li>
+   <li><a href="<?php $BASEPATH; ?>/music.php"><span>Music</span></a></li>
    <li><a href='#'><span>Other</span></a></li>
    <li><a href="<?php $BASEPATH; ?>/settings/settings.php"><span>Settings</span></a></li>
-   <li style="float: right;"><a href="logout.php"><span>Logout</span></a></li>
+   <li style="float: right;"><a href="<?php $BASEPATH; ?>/logout.php"><span>Logout</span></a></li>
 </ul>
 </div>
 </div>
