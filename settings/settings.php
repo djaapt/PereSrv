92444ed5c3fd32c273acc594556b63de373c87e0
Rocketcandy <andrewthooft@gmail.com>
2014-04-03 22:34:47 -0500

    Include footer again

--- settings/settings.php
+++ settings/settings.php
@@ -3,10 +3,10 @@
 include_once '../header.php';?>
 
 <div id="info">
-<?php echo $BASEPATH; ?>
+<?php echo BASEPATH; ?>
 Scan TV Show Folders: <a href="scan.php">SCAN TV Shows</a><br>
 Scan Movie Folders: <a href="moviescan.php">SCAN Movies</a><br>
 Scan Music Folders: <a href="musicscan.php">SCAN Music</a><br><br>
 Users: <a href="settings_users.php">Users</a>
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
-include $BASEPATH.'/footer.php'; ?>
+include BASEPATH.'/footer.php'; ?>
