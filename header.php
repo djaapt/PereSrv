6538a96e4919caa787f792dd2ab431fbfd367562
Rocketcandy <andrewthooft@gmail.com>
2014-04-03 22:30:01 -0500

    echo basepath value?

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ if(!session_is_registered(USER)){
 header("location:$BASEPATH/main_login.php");
 }
 //Include config file do not include this part in every page
-include '$BASEPATH/config.php';
+include echo '$BASEPATH/config.php';
 ?>
 <html>
 <head>
