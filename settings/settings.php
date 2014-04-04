3f557a8abf16d94079116740fc5b189aa2e9f581
Rocketcandy <andrewthooft@gmail.com>
2014-04-03 22:11:39 -0500

    update settings to fix header

--- settings/settings.php
+++ settings/settings.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <?php //Include the header - header starts the html and body tags
-include_once '$BASEPATH/header.php';?>
+include_once $BASEPATH.'/header.php';?>
 
 <div id="info">
 Scan TV Show Folders: <a href="scan.php">SCAN TV Shows</a><br>
@@ -8,4 +8,4 @@ Scan Movie Folders: <a href="moviescan.php">SCAN Movies</a><br>
 Scan Music Folders: <a href="musicscan.php">SCAN Music</a><br><br>
 Users: <a href="settings_users.php">Users</a>
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
-include '$BASEPATH/footer.php'; ?>
+include $BASEPATH.'/footer.php'; ?>
