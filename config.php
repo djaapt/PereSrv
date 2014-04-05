42af7bbed693bb5c2c6a23886e2dffa84a178a1f
Rocketcandy <andrewthooft@gmail.com>
2014-04-04 22:16:11 -0500

    Update directions

--- config.php
+++ config.php
@@ -5,9 +5,10 @@ PereSrv Configuration File
 //Define base directory
 define('BASEPATH', dirname(__FILE__));
 
-/*Define website with now trailing /
-Examples: example.com
-media.test.com
+/*Define website with DO NOT ADD trailing /
+Make sure you have http:// or https://
+Examples: http://example.com
+https://media.test.com
 */
 define('BASEURL', 'https://djaapt.com:8443');
 ?>
