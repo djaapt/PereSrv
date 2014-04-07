6b8a0daf31f5b1bb97ada7f22e5ab79befe3e5af
Rocketcandy <andrewthooft@gmail.com>
2014-04-07 08:02:06 -0500

    Update form and when to use variabls

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 /*
 PereSrv Configuration File
 */
-//Define base directory
+//Define base directory for use if you need to reference the file location directly.  If you need to get back to the root site location use BASEURL
 define('BASEPATH', dirname(__FILE__));
 
-/*Define website with DO NOT ADD trailing /
+/*Define website with DO NOT ADD trailing / Use to reference the root location for website access do not use for media file location
 Make sure you have http:// or https://
 Examples: http://example.com
 https://media.test.com
--- settings/settings_users.php
+++ settings/settings_users.php
@@ -1,6 +1,14 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <?php //Include the header - header starts the html and body tags
-include '../header.php'; ?>
+include '../header.php'; 
+$FIRSTNAME = check_input($_POST['FirstName']);
+$LASTNAME = check_input($_POST['LastName']);
+$USERNAME = check_input($_POST['username']);
+$PASSWORD = check_input($_POST['password']);
+$EMAIL = check_input($_POST['Email']);
+$MAXRATING = check_input($_POST['MaxRating']);
+$ADMIN = check_input($_POST['Admin']);
+?>
 
 <div id="info">
 <form id='register' action='settings_users.php' method='post' accept-charset='UTF-8'>
@@ -8,21 +16,30 @@ include '../header.php'; ?>
 <legend>Register</legend>
 <input type='hidden' name='submitted' id='submitted' value='1'/>
 <label for='name' >First Name*: </label>
-<input type='text' name='name' id='name' maxlength="50" /><br><br>
+<input type='text' name='FirstName' id='FirstName' maxlength="50" /><br><br>
 <label for='name' >Last Name*: </label>
-<input type='text' name='name' id='name' maxlength="50" /><br><br>
-<label for='name' >Max Rating*: </label>
-<input type='text' name='name' id='name' maxlength="50" /><br><br>
-<label for='name' >Administrator?*: </label>
-<input type='text' name='name' id='name' maxlength="50" /><br><br>
-<label for='email' >Email Address*:</label>
-<input type='text' name='email' id='email' maxlength="50" /><br><br>
+<input type='text' name='LastName' id='LastName' maxlength="50" /><br><br>
 <label for='username' >UserName*:</label>
 <input type='text' name='username' id='username' maxlength="50" /><br><br>
 <label for='password' >Password*:</label>
 <input type='password' name='password' id='password' maxlength="50" />
+<label for='email' >Email Address*:</label>
+<input type='text' name='Email' id='Email' maxlength="50" /><br><br>
+<label for='name' >Max Rating*: </label>
+<input type='text' name='MaxRating' id='MaxRating' maxlength="50" /><br><br>
+<label for='name' >Administrator?*: </label>
+<input type='text' name='Admin' id='Admin' maxlength="50" /><br><br>
 <input type='submit' name='Submit' value='Submit' />
 </fieldset>
 </form>
+
+<?php
+function check_input($DATA){
+    $DATA = trim($DATA);
+    $DATA = stripslashes($DATA);
+    $DATA = htmlspecialchars($DATA);
+    return $DATA;
+}
+?>
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
 include BASEPATH.'/footer.php'; ?>
