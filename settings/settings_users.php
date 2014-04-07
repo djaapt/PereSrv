0271bd9e638b2f9c0289810b67c176066dd0a22a
Rocketcandy <andrewthooft@gmail.com>
2014-04-07 13:03:08 -0500

    Update insert statement

--- settings/settings_users.php
+++ settings/settings_users.php
@@ -51,7 +51,7 @@ if (isset ($_REQUEST['adding'])) {
 		$ADMIN = $_POST['Admin'];
 		
 		//Build insert statement
-		$QUERY = "insert into $TABLE values(NULL,'$USERNAME','$PASSWORD','$FIRSTNAME','$LASTNAME','$EMAIL','$MAXRATING','$ADMIN')";
+		$QUERY = "insert into $TABLE values(NULL,'$USERNAME','$PASSWORD','$FIRSTNAME','$LASTNAME','$EMAIL',NULL,NULL)";
 		echo $QUERY;
 		
 	}
