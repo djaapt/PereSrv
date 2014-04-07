81e27cc5b9ddbf050641f8e93e7fc46e5106c4d1
Rocketcandy <andrewthooft@gmail.com>
2014-04-07 08:38:45 -0500

    update form

--- settings/settings_users.php
+++ settings/settings_users.php
@@ -1,11 +1,12 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <?php //Include the header - header starts the html and body tags
-include '../header.php'; 
-$FIRSTNAME = check_input($_POST['FirstName']);
-$LASTNAME = check_input($_POST['LastName']);
-$USERNAME = check_input($_POST['username']);
-$PASSWORD = check_input($_POST['password']);
-$EMAIL = check_input($_POST['Email']);
+include_once '../header.php'; 
+include_once '/media/dbinfo.php';
+$FIRSTNAME = check_input($_POST['FirstName'],"Enter First Name!");
+$LASTNAME = check_input($_POST['LastName'],"Enter Last Name!");
+$USERNAME = check_input($_POST['username'],"Enter Userame!");
+$PASSWORD = check_input($_POST['password'],"Enter Password!");
+$EMAIL = check_input($_POST['Email'],"Enter Email Address!");
 $MAXRATING = check_input($_POST['MaxRating']);
 $ADMIN = check_input($_POST['Admin']);
 ?>
@@ -15,31 +16,36 @@ $ADMIN = check_input($_POST['Admin']);
 <fieldset>
 <legend>Register</legend>
 <input type='hidden' name='submitted' id='submitted' value='1'/>
-<label for='name' >First Name*: </label>
+<label for='FirstName' >First Name*: </label>
 <input type='text' name='FirstName' id='FirstName' maxlength="50" /><br><br>
-<label for='name' >Last Name*: </label>
+<label for='LastName' >Last Name*: </label>
 <input type='text' name='LastName' id='LastName' maxlength="50" /><br><br>
 <label for='username' >UserName*:</label>
 <input type='text' name='username' id='username' maxlength="50" /><br><br>
 <label for='password' >Password*:</label>
-<input type='password' name='password' id='password' maxlength="50" />
-<label for='email' >Email Address*:</label>
+<input type='password' name='password' id='password' maxlength="50" /><br><br>
+<label for='Email' >Email Address*:</label>
 <input type='text' name='Email' id='Email' maxlength="50" /><br><br>
-<label for='name' >Max Rating*: </label>
-<input type='text' name='MaxRating' id='MaxRating' maxlength="50" /><br><br>
-<label for='name' >Administrator?*: </label>
-<input type='text' name='Admin' id='Admin' maxlength="50" /><br><br>
+<select name='MaxRating'>
+	<option value="100" selected>All</option>
+</select>
+<input type='checkbox' name='Admin' value="1">Administrator<br><br>
 <input type='submit' name='Submit' value='Submit' />
 </fieldset>
 </form>
 
 <?php
-function check_input($DATA){
+function check_input($DATA, $ERROR='')
+{
     $DATA = trim($DATA);
     $DATA = stripslashes($DATA);
     $DATA = htmlspecialchars($DATA);
+	if ($ERROR && strlen($DATA) == 0)
+	{
+		die($ERROR);
+	}
     return $DATA;
 }
 ?>
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
-include BASEPATH.'/footer.php'; ?>
+include_once BASEPATH.'/footer.php'; ?>
