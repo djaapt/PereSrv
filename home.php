84928fa78c5040555e0dbea9f8b8e99ccd63825f
Rocketcandy <andrewthooft@gmail.com>
2014-04-07 17:58:11 -0500

    Remove home.php as index.php replaced it

--- home.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<?php //Include the header - header starts the html and body tags
-include_once 'header.php'; ?>
-
-<div id="info">
-HOME
-
-<?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
-include_once 'footer.php'; ?>
