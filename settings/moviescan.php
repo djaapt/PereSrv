b0bb9cc81f0399bfdebacca734abbdf3b6381311
Rocketcandy <andrewthooft@gmail.com>
2014-04-08 19:27:27 -0500

    Update header path so we get BASEPATH defined

--- settings/moviescan.php
+++ settings/moviescan.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <?php //Include the header - header starts the html and body tags
-include 'header.php'; ?>
+include '../header.php'; ?>
 
 <div id="info">
 Back To Settings: <a href="settings.php"><span>Settings</span></a><br><br>
@@ -92,4 +92,4 @@ else
 ?>
 
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
-include 'footer.php'; ?>
+include BASEURL.'/footer.php'; ?>
