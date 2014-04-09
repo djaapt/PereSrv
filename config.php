f8ca89068bbf47ea567e779a1ae008a1d65d29cf
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-04-09 09:52:09 -0500

    Undo all those

--- config.php
+++ config.php
@@ -3,8 +3,7 @@
 PereSrv Configuration File
 */
 //Define base directory for use if you need to reference the file location directly.  If you need to get back to the root site location use BASEURL
-//define('BASEPATH', dirname(__FILE__));
-define('BASEPATH', /);
+define('BASEPATH', dirname(__FILE__));
 
 /*Define website with DO NOT ADD trailing / Use to reference the root location for website access do not use for media file location
 Examples: example.com
