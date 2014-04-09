14d2ce2cb2f0efc41439db62868e3d5381a2126a
symalla <symalla@gmail.com>
2014-04-09 09:00:06 -0500

    hopefully not breaking navbar

--- header.php
+++ header.php
@@ -16,14 +16,49 @@ header("location:".BASEURL."/main_login.php");
 <body>
 <div id="header">
 <div id='cssmenu'>
-<ul>
-   <li class='active'><a href="<?php BASEPATH; ?>/index.php"><span>Home</span></a></li>
-   <li><a href="<?php BASEPATH; ?>/movies.php"><span>Movies</span></a></li>
-   <li><a href="<?php BASEPATH; ?>/shows.php"><span>TVShows</span></a></li>
-   <li><a href="<?php BASEPATH; ?>/music.php"><span>Music</span></a></li>
-   <li><a href='#'><span>Other</span></a></li>
-   <li><a href="<?php BASEPATH; ?>/settings/settings.php"><span>Settings</span></a></li>
-   <li style="float: right;"><a href="<?php BASEPATH; ?>/logout.php"><span>Logout</span></a></li>
-</ul>
+<?php
+//Build the connection to SQL server
+include '/media/dbinfo.php';
+$username = $_SESSION['username'];
+//DB connection variable to call later
+$db = new mysqli($HOST,$USER,$PASS,$DBASE);
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+$TABLE = "members";
+$sql = <<<SQL
+    SELECT `Admin`
+    FROM `$TABLE`
+    WHERE `username` = "$username" 
+SQL;
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+while($row = $result->fetch_assoc()){
+	$var = $row['Admin'];
+}
+if($var==1) {
+echo '<ul>';
+echo '<li class='active'><a href="<?php BASEPATH; ?>/index.php"><span>Home</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/movies.php"><span>Movies</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/shows.php"><span>TVShows</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/music.php"><span>Music</span></a></li>';
+echo '<li><a href='#'><span>Other</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/settings/settings.php"><span>Settings</span></a></li>';
+echo '<li style="float: right;"><a href="<?php BASEPATH; ?>/logout.php"><span>Logout</span></a></li>';
+echo '</ul>';
+}
+else{
+echo '<ul>';
+echo '<li class='active'><a href="<?php BASEPATH; ?>/index.php"><span>Home</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/movies.php"><span>Movies</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/shows.php"><span>TVShows</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/music.php"><span>Music</span></a></li>';
+echo '<li><a href='#'><span>Other</span></a></li>';
+echo '<li style="float: right;"><a href="<?php BASEPATH; ?>/logout.php"><span>Logout</span></a></li>';
+echo '</ul>';
+}
+?>
 </div>
 </div>
