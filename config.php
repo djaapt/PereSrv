7038fbed62bfda2817ae8dcae4c5521cf4a67822
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-04-09 09:51:11 -0500

    maybe this way?

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ PereSrv Configuration File
 */
 //Define base directory for use if you need to reference the file location directly.  If you need to get back to the root site location use BASEURL
 //define('BASEPATH', dirname(__FILE__));
-define('BASEPATH', '/');
+define('BASEPATH', /);
 
 /*Define website with DO NOT ADD trailing / Use to reference the root location for website access do not use for media file location
 Examples: example.com
