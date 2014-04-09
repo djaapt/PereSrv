9452c507205f7b2b37843b7513f14c2b60a01dd0
Rocketcandy <andrewthooft@gmail.com>
2014-04-08 19:54:08 -0500

    update the function instead maybe

--- settings/scan.php
+++ settings/scan.php
@@ -9,12 +9,12 @@ Back To Settings: <a href="settings.php"><span>Settings</span></a><br><br>
 $EXCLUDE_LIST = array(".","..",".htaccess","index.php","fileNice");
 $EXCLUDE_LIST_PRINTABLE = implode(", ", $EXCLUDE_LIST);
 //Unwanted characters in query for later
-/*function clean_up( $TEXT ){
+function clean_up($TEXT){
 	//Add any other characters to be removed inside the array
-	$FIXAPOSTROPHE = array("'",);
-	return str_ireplace($FIXAPOSTROPHE, "''", $TEXT);
+	//$FIXAPOSTROPHE = array("'",);
+	return htmlspecialchars($TEXT);
 }
-*/
+
 //Build the connection to SQL server
 include '/media/dbinfo.php';
 
@@ -26,11 +26,11 @@ $DBC = mysqli_connect($HOST,$USER,$PASS,$DBASE) or die ('Unable to select Databa
 $TVSHOWDIR = BASEPATH."/Seasons/";
 
 //Scans the directory and runs it through the clean-up function we created
-$TVFILES = /*clean_up*/htmlspecialchars(array_diff(scandir($TVSHOWDIR),$EXCLUDE_LIST));
+$TVFILES = clean_up(array_diff(scandir($TVSHOWDIR),$EXCLUDE_LIST));
 
 //This one is just for testing, we won't use it in the final version
 echo "Files in the Directory $TVSHOWDIR not showing the excluded list: $EXCLUDE_LIST_PRINTABLE:<br><br>";
-$TVFILES1 = implode(", ", /*clean_up*/htmlspecialchars(array_diff(scandir($TVSHOWDIR),$EXCLUDE_LIST)));
+$TVFILES1 = implode(", ", clean_up(array_diff(scandir($TVSHOWDIR),$EXCLUDE_LIST)));
 
 //Print results from the test array
 echo "$TVFILES1";
