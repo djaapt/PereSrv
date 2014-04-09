ae465a34bf9d7e007349e387df265c6a62370c54
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-04-09 13:49:41 -0500

    Set the entire thing in an else statement?

--- main_login.php
+++ main_login.php
@@ -1,32 +1,3 @@
-<body bgcolor="#000000">
-<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#B0B0B0">
-<tr>
-<form name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-<td>
-<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#16C9C9">
-<tr>
-<td colspan="3"><strong>Member Login </strong></td>
-</tr>
-<tr>
-<td width="78">Username</td>
-<td width="6">:</td>
-<td width="294"><input name="USERNAME" type="text" id="USERNAME"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td>:</td>
-<td><input name="PASSWORD" type="password" id="PASSWORD"></td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td><input type="submit" name="Submit" value="Login"></td>
-</tr>
-</table>
-</td>
-</form>
-</tr>
-</table>
 <?php
 if(isset($_POST['Submit'])) {
 	//Build the connection to SQL server
@@ -66,5 +37,37 @@ if(isset($_POST['Submit'])) {
 		echo "<font color=\"#16C9C9\">Wrong Username or Password</font>";
 	}
 }
+else {
 ?>
-</body>
+	<body bgcolor="#000000">
+	<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#B0B0B0">
+	<tr>
+	<form name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<td>
+	<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#16C9C9">
+	<tr>
+	<td colspan="3"><strong>Member Login </strong></td>
+	</tr>
+	<tr>
+	<td width="78">Username</td>
+	<td width="6">:</td>
+	<td width="294"><input name="USERNAME" type="text" id="USERNAME"></td>
+	</tr>
+	<tr>
+	<td>Password</td>
+	<td>:</td>
+	<td><input name="PASSWORD" type="password" id="PASSWORD"></td>
+	</tr>
+	<tr>
+	<td>&nbsp;</td>
+	<td>&nbsp;</td>
+	<td><input type="submit" name="Submit" value="Login"></td>
+	</tr>
+	</table>
+	</td>
+	</form>
+	</tr>
+	</table>
+	</body>
+<?php }
+?>
