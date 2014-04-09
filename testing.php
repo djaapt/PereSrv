bb51d9aa3454839e8bfdcd79db3fe47cac21d533
symalla <symalla@gmail.com>
2014-04-09 07:54:36 -0500

    test db pull via session user

--- testing.php
+++ testing.php
@@ -2,9 +2,19 @@
 <?php //Include the header - header starts the html and body tags
 include_once 'header.php'; ?>
 
-<?php 
+<?php
 session_start();
-echo $_SESSION['username'];
+$username = $_SESSION['username'];
+//Build the connection to SQL server
+include '/media/dbinfo.php';
+
+//DB connection variable to call later
+$TABLE = "members";
+$DBC = mysqli_connect($HOST,$USER,$PASS,$DBASE) or die ('Unable to select Database');
+
+$query="SELECT * FROM $TABLE where username=$username";
+$result=mysql_query($query);
+echo $result;
 ?>
 
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
