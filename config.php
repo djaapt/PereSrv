0aa8eb063e0b13457bee3f323721cbca647a9f09
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-04-09 09:49:45 -0500

    Update basepath var

--- config.php
+++ config.php
@@ -3,7 +3,8 @@
 PereSrv Configuration File
 */
 //Define base directory for use if you need to reference the file location directly.  If you need to get back to the root site location use BASEURL
-define('BASEPATH', dirname(__FILE__));
+//define('BASEPATH', dirname(__FILE__));
+define('BASEPATH', '/');
 
 /*Define website with DO NOT ADD trailing / Use to reference the root location for website access do not use for media file location
 Examples: example.com
