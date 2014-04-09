624f0de84fdb695616420a65e2007a42b035cfe5
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-04-09 13:19:36 -0500

    Ok lets try it this way

--- main_login.php
+++ main_login.php
@@ -1,7 +1,7 @@
 <body bgcolor="#000000">
 <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#B0B0B0">
 <tr>
-<form name="form1" method="post" action="main_login.php?logincheck=1">
+<form name="form1" method="post" action="main_login.php">
 <td>
 <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#16C9C9">
 <tr>
@@ -28,46 +28,42 @@
 </tr>
 </table>
 <?php
-if (isset ($_REQUEST[''])) {
-	$LOGINCHECK = $_REQUEST['logincheck'];
-	if ($LOGINCHECK == 1) {
-		//Build the connection to SQL server
-		include_once '/media/dbinfo.php';
-		$TABLE = "members"; // Table name 
+$error = "<font color=\"#16C9C9\">Wrong Username or Password</font>";
+//Build the connection to SQL server
+include_once '/media/dbinfo.php';
+$TABLE = "members"; // Table name 
 
-		// Connect to server and select databse.
-		mysql_connect("$HOST", "$USER", "$PASS")or die("cannot connect"); 
-		mysql_select_db("$DBASE")or die("cannot select DB");
+// Connect to server and select databse.
+mysql_connect("$HOST", "$USER", "$PASS")or die("cannot connect"); 
+mysql_select_db("$DBASE")or die("cannot select DB");
 
-		// username and password sent from form 
-		$USERNAME=$_POST['USERNAME']; 
-		$PASSWORD=$_POST['PASSWORD']; 
+// username and password sent from form 
+$USERNAME=$_POST['USERNAME']; 
+$PASSWORD=$_POST['PASSWORD']; 
 
-		// To protect MySQL injection (more detail about MySQL injection)
-		$USERNAME = stripslashes($USERNAME);
-		$PASSWORD = stripslashes($PASSWORD);
-		$USERNAME = mysql_real_escape_string($USERNAME);
-		$PASSWORD = mysql_real_escape_string($PASSWORD);
-		$PASSWORD = md5($PASSWORD);
-		$SQL="SELECT * FROM $TABLE WHERE username='$USERNAME' and password='$PASSWORD'";
-		$result=mysql_query($SQL);
+// To protect MySQL injection (more detail about MySQL injection)
+$USERNAME = stripslashes($USERNAME);
+$PASSWORD = stripslashes($PASSWORD);
+$USERNAME = mysql_real_escape_string($USERNAME);
+$PASSWORD = mysql_real_escape_string($PASSWORD);
+$PASSWORD = md5($PASSWORD);
+$SQL="SELECT * FROM $TABLE WHERE username='$USERNAME' and password='$PASSWORD'";
+$result=mysql_query($SQL);
 
-		// Mysql_num_row is counting table row
-		$count=mysql_num_rows($result);
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
 
-		// If result matched $USERNAME and $PASSWORD, table row must be 1 row
-		if($count==1){
-			// Register $USERNAME, $PASSWORD and redirect to file "index.php"
-			session_start();
-			$_SESSION['username'] = $USERNAME;
-			session_register("USERNAME"); 
-			session_register("PASSWORD"); 
-			header("location:index.php");
-		}
-		else {
-			echo "<font color=\"#16C9C9\">Wrong Username or Password</font>";
-		}
-	}
+// If result matched $USERNAME and $PASSWORD, table row must be 1 row
+if($count==1){
+	// Register $USERNAME, $PASSWORD and redirect to file "index.php"
+	session_start();
+	$_SESSION['username'] = $USERNAME;
+	session_register("USERNAME"); 
+	session_register("PASSWORD"); 
+	header("location:index.php");
+}
+if(isset($error)) {
+    echo $error;
 }
 ?>
 </body>
