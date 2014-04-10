5138bc364be5de1e178c70b0973bcd68e477737d
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-04-10 12:15:14 -0500

    Fix the reset

--- header.php
+++ header.php
@@ -41,22 +41,22 @@ while($row = $result->fetch_assoc()){
 if($var==1) {
 echo '<ul>';
 echo '<li class="active"><a href="<?php BASEPATH; ?>/index.php"><span>Home</span></a></li>';
-echo '<li><a href="'.BASEURL.'/movies.php"><span>Movies</span></a></li>';
-echo '<li><a href="'.BASEURL.'/shows.php"><span>TVShows</span></a></li>';
-echo '<li><a href="'.BASEURL.'/music.php"><span>Music</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/movies.php"><span>Movies</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/shows.php"><span>TVShows</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/music.php"><span>Music</span></a></li>';
 echo '<li><a href="#"><span>Other</span></a></li>';
-echo '<li><a href="'.BASEURL.'/settings/settings.php"><span>Settings</span></a></li>';
-echo '<li style="float: right;"><a href="'.BASEURL.'/logout.php"><span>Logout</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/settings/settings.php"><span>Settings</span></a></li>';
+echo '<li style="float: right;"><a href="<?php BASEPATH; ?>/logout.php"><span>Logout</span></a></li>';
 echo '</ul>';
 }
 else{
 echo '<ul>';
-echo '<li class="active"><a href="'.BASEURL.'/index.php"><span>Home</span></a></li>';
-echo '<li><a href="'.BASEURL.'/movies.php"><span>Movies</span></a></li>';
-echo '<li><a href="'.BASEURL.'/shows.php"><span>TVShows</span></a></li>';
-echo '<li><a href="'.BASEURL.'/music.php"><span>Music</span></a></li>';
+echo '<li class="active"><a href="<?php BASEPATH; ?>/index.php"><span>Home</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/movies.php"><span>Movies</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/shows.php"><span>TVShows</span></a></li>';
+echo '<li><a href="<?php BASEPATH; ?>/music.php"><span>Music</span></a></li>';
 echo '<li><a href="#"><span>Other</span></a></li>';
-echo '<li style="float: right;"><a href="'.BASEURL.'/logout.php"><span>Logout</span></a></li>';
+echo '<li style="float: right;"><a href="<?php BASEPATH; ?>/logout.php"><span>Logout</span></a></li>';
 echo '</ul>';
 }
 ?>
