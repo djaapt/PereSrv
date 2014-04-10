cc403853ccbc848984d77b1276ba29610b4d1b3d
Rocketcandy <andrewthooft@gmail.com>
2014-04-09 22:22:50 -0500

    maybe if I break it all the way out

--- main_login.php
+++ main_login.php
@@ -61,7 +61,12 @@ if(isset($_POST['Submit']) && $_POST['Submit']=='Login') {
 <tr>
 <td>&nbsp;</td>
 <td>&nbsp;</td>
-<?php if($error){ echo "<tr>$error</tr><td>&nbsp;</td><td>&nbsp;</td>"; } ?>
+<?php if($error){ 
+	echo '<td>'.$error.'</td>';
+	echo '<td>&nbsp;</td>';
+	echo '<td>&nbsp;</td>'; 
+	} 
+?>
 <td><input type="submit" name="Submit" value="Login"></td>
 </tr>
 </table>
