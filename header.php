4232e3f6c053debf6382283d456e7edaa6ae9cc5
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-04-10 12:19:35 -0500

    remove baseanything from main login

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 include_once 'config.php';
 session_start();
 if(!session_is_registered('USERNAME')){
-header("location:<?php BASEPATH; ?>/main_login.php");
+header("location:main_login.php");
 }
 ?>
 <html>
