dfa6ec3f5775ccc7d9d6d5fb00783fcb62a4337b
Rocketcandy <andrewthooft@gmail.com>
2014-04-13 17:07:24 -0500

    Helps if you actually use the variable

--- settings/scan.php
+++ settings/scan.php
@@ -84,7 +84,7 @@ if (empty($REMOVEDUPS)) {
 	$TVQUERY = "INSERT INTO $TABLE";
 	//Comma separates each value and add single quotes(') around each value
 	$TVQUERY .= " VALUES (NULL,".implode("),(NULL,", $REMOVEDUPS).") ";
-	clean_up($REMOVEDUPS);
+	$REMOVEDUPS = clean_up($REMOVEDUPS);
 	$TEST = implode(",",$REMOVEDUPS);
 	//Print the query
 	echo $TEST."<br>";
