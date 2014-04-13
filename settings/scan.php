b4774b243937df27e590237a351cf4502232dcd2
Rocketcandy <andrewthooft@gmail.com>
2014-04-13 16:42:45 -0500

    Test

--- settings/scan.php
+++ settings/scan.php
@@ -78,7 +78,7 @@ if (empty($REMOVEDUPS)) {
 	$TVQUERY = "INSERT INTO $TABLE";
 	//Comma separates each value and add single quotes(') around each value
 	$TVQUERY .= " VALUES (NULL,".implode("),(NULL,", $REMOVEDUPS).") ";
-	$TEST = "(NULL,".implode(",",$REMOVEDUPS).") ";
+	$TEST = "(NULL,".implode("TEST,",$REMOVEDUPS).") ";
 	//Print the query
 	echo $TEST."<br>";
 	echo $TVQUERY;
