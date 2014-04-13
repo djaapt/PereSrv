cf1bcafc0bcfc4803c29cb5ed3b6a35259d3325e
Rocketcandy <andrewthooft@gmail.com>
2014-04-13 15:58:23 -0500

    Print change

--- settings/scan.php
+++ settings/scan.php
@@ -74,6 +74,7 @@ if (empty($REMOVEDUPS)) {
 	$DBC = mysqli_connect($HOST,$USER,$PASS,$DBASE) or die ('Unable to select Database');
 	echo implode(",",$REMOVEDUPS);
 	if (in_array(",",$REMOVEDUPS)) { str_replace(",","\\,",$REMOVEDUPS);
+	echo $REMOVEDUPS;
 	}
 	echo "<br><br>";
 	//Build the query your going to use
