1f9f0461ed78fe474dfbddf2a4436a95da70c536
symalla <symalla@gmail.com>
2014-04-18 09:51:54 -0500

    adding echo to write html lines

--- player.php
+++ player.php
@@ -3,10 +3,10 @@
 include 'header.php'; ?>
 <div id="info">
 <?php
-echo  '.$_GET["name"].'
-<video height="340" width="480" controls poster="images/logo.png">
-<source src="Videos/'.$_GET["name"].'" type="video/webm">
-</video>
+echo  '.$_GET["name"].';
+echo '<video height="340" width="480" controls poster="images/logo.png">';
+echo '<source src="Videos/'.$_GET["name"].'" type="video/webm">';
+echo '</video>';
 ?> 
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
 include 'footer.php'; ?>
