1180216b549c84b1fc812a89b80bd3d8f27c31f0
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-04-18 13:32:18 -0500

    Add movie count

--- movies.php
+++ movies.php
@@ -12,6 +12,10 @@ include '/media/dbinfo.php';
 $TABLE = "movies";
 $DBC = mysqli_connect($HOST,$USER,$PASS,$DBASE) or die ('Unable to select Database');
 
+//List Number of Movies
+$MOVCOUNT = mysqli_num_rows(mysqli_query($DBC, "SELECT moviename FROM $TABLE"));
+echo "Total number of Movies: $MOVCOUNT<br><br>";
+
 //Build query to check for duplicates already in the database
 $QUERY = "SELECT moviename FROM $TABLE";
 $GET = mysqli_query($DBC,$QUERY);
@@ -20,7 +24,6 @@ $MEDIA = $ROW['moviename'];
 $PATH = "/Videos/".$MEDIA;
 echo '<a href="player.php?name='.$MEDIA.'">'.$MEDIA.'</a></br>';
 }
-
 mysqli_close($DBC);
 
 //Include the footer - The footer ends the body and html tags </div> tag ends in footer
