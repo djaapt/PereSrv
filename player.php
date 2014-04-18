5fb34337ef0f5962f179a4c36c28688f3472ffae
symalla <symalla@gmail.com>
2014-04-18 09:50:34 -0500

    fixing syntax error in player.php
    PHP Parse error:  syntax error, unexpected ']', expecting ',' or ';' in
    /var/www/PereSrv/player.php on line 6

--- player.php
+++ player.php
@@ -3,7 +3,7 @@
 include 'header.php'; ?>
 <div id="info">
 <?php
-echo  '. $_GET["name'] .'
+echo  '.$_GET["name"].'
 <video height="340" width="480" controls poster="images/logo.png">
 <source src="Videos/'.$_GET["name"].'" type="video/webm">
 </video>
