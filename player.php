7f939c40af9d542ce3c14764c7c5d323b24df39e
symalla <symalla@gmail.com>
2014-04-18 09:58:44 -0500

    updating movie title placement

--- player.php
+++ player.php
@@ -4,7 +4,7 @@ include 'header.php'; ?>
 <div id="info">
 <?php
 $Title = $_GET["name"]; 
-echo  "$Title";
+echo  "<H1>$Title</H1><br>";
 echo '<video height="340" width="480" controls poster="images/logo.png">';
 echo '<source src="Videos/'.$Title.'" type="video/webm">';
 echo '</video>';
