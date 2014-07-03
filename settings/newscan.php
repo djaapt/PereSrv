16d5c29c4c997d0bc706ca8ad0c6e7f58ddbc2f7
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-07-03 10:08:45 -0500

    Get header and footer added to new scan page

---
+++ settings/newscan.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<?php //Include the header - header starts the html and body tags
+include '../header.php'; ?>
+
+<div id="info">
+Back To Settings: <a href="settings.php"><span>Settings</span></a><br><br>
+
+
+
+<?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
+include '../footer.php'; ?>
