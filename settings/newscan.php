a4ee7f8e4ca0e35c7e20ab784f510bc860a2d87e
Rocketcandy <andrewthooft@gmail.com>
2014-07-09 18:13:34 -0500

    Fix CSS to see if we can get text on top

--- settings/newscan.php
+++ settings/newscan.php
@@ -7,19 +7,6 @@ Back To Settings: <a href="settings.php"><span>Settings</span></a><br><br>
 
 <!-- Define what folders have what type of media in them -->
 <?php
-/*function ListDirFile($DIR){
-    $RFS = scandir($DIR);
-    echo '<ol>';
-    foreach($RFS as $FS){
-        if($FS != '.' && $FS != '..'){
-            echo '<li>'.$FS;
-            if(is_dir($DIR.'/'.$FS)) ListDirFile($DIR.'/'.$FS);
-            echo '</li>';
-        }
-    }
-    echo '</ol>';
-}*/
-
 $SHOWS = "../Seasons/24";
 $MOVIES = "../Videos";
 $MUSIC = "../Music";
@@ -51,16 +38,6 @@ function getDirContents($dir)
   return $contents;
 }
 
-/*$TEST = explode(listDirFile("$SHOWS"));
-$iterator = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator($MOVIES),
-   RecursiveIteratorIterator::SELF_FIRST
-);
-foreach ($iterator as $fileObject) {
-    $files[] = $fileObject;
-    // or if you only want the filenames
-    //$files[] = $fileObject->getPathname();
-}*/
 $files = getDirContents($SHOWS);
 echo implode(",",$files);
 ?>
