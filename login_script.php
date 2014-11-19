4c5d33e8f1ee347e0832444cd191170dcd2d6d65
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-11-19 10:16:07 -0600

    Update to see if $error works
    Test to see if error is getting pulled from login_script.php to
    main_login.php

--- login_script.php
+++ login_script.php
@@ -2,7 +2,7 @@
 <?php
 include('/media/dbinfo.php');
 session_start(); //Starting Session
-$error=''; //Variable to store error message
+$error="<font color=\"#16C9C9\">Is this showing at all?</font>"; //Variable to store error message
 if (isset($_POST['submit'])) {
 	if (empty($_POST['username']) || empty($_POST['password'])) {
 		$error = "<font color=\"#16C9C9\">Wrong Username or Password</font>";
