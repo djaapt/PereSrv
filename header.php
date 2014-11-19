0a525d246ff6f7aa640819838b6eefbf79caaa57
Rocketcandy <Rocketcandy@users.noreply.github.com>
2014-11-19 10:46:18 -0600

    Fix Order

--- header.php
+++ header.php
@@ -42,8 +42,8 @@ echo '<li><a href="/shows.php"><span>TVShows</span></a></li>';
 echo '<li><a href="/music.php"><span>Music</span></a></li>';
 echo '<li><a href="#"><span>Other</span></a></li>';
 echo '<li><a href="/settings/settings.php"><span>Settings</span></a></li>';
-echo '<li style="float: right;"><a href="profile.php">'.$login_session.'<span></span></a></li>';
 echo '<li style="float: right;"><a href="/logout.php"><span>Logout</span></a></li>';
+echo '<li style="float: right;"><a href="profile.php">'.$login_session.'<span></span></a></li>';
 echo '</ul>';
 }
 else{
@@ -53,8 +53,8 @@ echo '<li><a href="/movies.php"><span>Movies</span></a></li>';
 echo '<li><a href="/shows.php"><span>TVShows</span></a></li>';
 echo '<li><a href="/music.php"><span>Music</span></a></li>';
 echo '<li><a href="#"><span>Other</span></a></li>';
-echo '<li style="float: right;"><a href="profile.php">'.$login_session.'<span></span></a></li>';
 echo '<li style="float: right;"><a href="/logout.php"><span>Logout</span></a></li>';
+echo '<li style="float: right;"><a href="profile.php">'.$login_session.'<span></span></a></li>';
 echo '</ul>';
 }
 ?>
--- profile.php
+++ profile.php
@@ -3,18 +3,9 @@
 include('header.php'); // Includes header
 include('session.php');
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Your Home Page</title>
-<link href="style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<div id="profile">
-<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
-<b id="logout"><a href="logout.php">Log Out</a></b>
-</div>
-</body>
-</html>
+
+Your Home Page <br><br>
+Welcome : <?php echo $login_session; ?>
+
 <?php //Include the footer - The footer ends the body and html tags </div> tag ends in footer
 include_once 'footer.php'; ?>
